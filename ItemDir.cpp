422e4f324b1b128a3896c17a71ac4d42ac4a4711
dewse <otacon911@hotmail.com>
2017-11-22 21:51:10 -0500

    Add files via upload

---
+++ ItemDir.cpp
@@ -0,0 +1,12 @@
+Class ALC{
+  public:
+    double element(water, lightning);
+    double
+};
+
+
+Var Horn Glue{
+  public
+    double
+
+};
---
+++ main.cpp
@@ -0,0 +1,87 @@
+
+/*#include "ItemDir.cpp"*/
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+int myVar = 10;
+int myVarLimit = 50;
+int cin01;
+int main() {
+
+int num =1;
+int number;
+int total = 0;
+
+int a = 42;
+do {
+  cout << a << endl;
+  a++;
+} while(a < 5);
+
+
+
+
+
+/*int main() {
+/*  ofstream myfile;
+  myfile.open ("temp_item.txt");
+  myfile << "New addition to file.\n";
+  myfile.close();
+  std::cout << "Hello World v1!" <<endl;
+  return 0;
+}
+
+int main(){
+  ofstream myfile;
+    myfile.open ("temp_item2.txt");
+    myfile << "New addition to file2.\n";
+    myfile.close();
+}
+*/
+
+/*
+int main(){
+int a=3;
+int b=2;
+b=a++;
+cout<<" b=a++| b is now "<<b<<endl;
+cout<<" b=a++| a is now "<<a<<endl;
+cout<<" b+a  | b is now "<<b+a<<endl;
+cout<<" b++  | b is now "<<b++<<endl;
+cout<<" ++b  | b is now "<<++b<<endl;
+cout<<" b++  | b is now "<<b++<<endl;
+cout<<" b++  | b is now "<<b++<<endl;
+cout<<" b++  | a is now "<<a<<endl;
+while (b<10) {
+cout<< "b is at " << b << endl;
+b++;
+}
+}
+*/
+/*
+int main(){
+  cout << "myVar is " <<myVar;
+  myVar++;
+  cout << "myVar is " <<myVar;
+  myVar <= myVarLimit;
+  cout << "int myVar is " <<myVar;
+  cout << "cin was " <<cin01;
+  myVar = 25;
+  cout << "myVar is once again " <<myVar;
+  myVar += myVar;
+  cout << "myVar is " <<myVar++;
+  return 0;
+}
+
+/*int hw2() {
+  std::cout << "Hello World v2!" <<endl;
+  return 0;
+}
+*/
+
+/* Comment
+g++ -o c002.exe main.cpp
+
+*/
+}
